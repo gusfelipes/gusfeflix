b3ad5e379a1ba73a195a2d42ed9439edd0e79f04
gustavo <gusfelipes@gmail.com>
2020-07-29 16:01:28 -0300

    segundo dia da imersao

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,14 @@
 import React from "react";
-import Menu from "./components/Menu";
-import dadosIniciais from "./data/dados_iniciais.json";
-import BannerMain from "./components/BannerMain";
-import Carousel from "./components/Carousel";
-import Footer from "./components/Footer";
+// import Menu from "../../components/Menu";
+import dadosIniciais from "../../data/dados_iniciais.json";
+import BannerMain from "../../components/BannerMain";
+import Carousel from "../../components/Carousel";
+// import Footer from "../../components/Footer";
+import PageDefault from "../../components/PageDefault";
 
-function App() {
+function Home() {
   return (
-    <div>
-      <Menu />
+    <PageDefault>
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
@@ -20,9 +20,8 @@ function App() {
       <Carousel ignoreFirstVideo category={dadosIniciais.categorias[3]} />
       <Carousel ignoreFirstVideo category={dadosIniciais.categorias[4]} />
       <Carousel ignoreFirstVideo category={dadosIniciais.categorias[5]} />
-      <Footer />
-    </div>
+    </PageDefault>
   );
 }
 
-export default App;
+export default Home;
